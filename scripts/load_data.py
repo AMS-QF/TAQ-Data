3d4535c5be4db41fa69041ae8a7dbab9cc32b9e5
Jason Bohne <jbohne822@gmail.com>
2023-01-24 13:49:25 -0500

    remove data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -24,8 +24,8 @@ def get_trades(conn: Connection, exchange: str, symbol: str, start: str, end: st
     """Get trades from the database"""
 
     result, path = conn.client_get_trades(exchange, symbol, start, end, data_dir)
-
-    trades = pd.read_csv(path)
+    print(path)
+    trades = pd.read_csv("../" + path)
 
     trades = clean_trades(trades)
     trades.to_csv(path)
