cbcaf14aefb25767dcdeaa1346d3e5e68b716e84
Jason Bohne <jbohne822@gmail.com>
2023-02-03 14:50:09 -0500

    added infra for feature gen

---
+++ scripts/feature_gen/list_features.py
@@ -0,0 +1,96 @@
+import pandas as pd
+
+
+def list_features(raw_data=False, trades=True, quotes=True, names_only=False):
+    """List all features available"""
+
+    trade_features = {}
+    quote_features = {}
+
+    if raw_data:
+        if trades:
+
+            raw_trade_features = {
+                "Exchange": "Exchange of Trade",
+                "Symbol": "Symbol pf Trade",
+                "Trade_Volume": "Trade Volume",
+                "Trade_Price": "Trade Price",
+                "Sale_Condition": "Condition of the Trade",
+                "Source_of_Trade": "Source which generated the Trade",
+                "Trade_Correction_Indicator": "Indicates if the trade is a correction",
+                "Sequence_Number": "Sequence Number of the Trade",
+                "Trade_Id": "Trade ID",
+                "Trade_Reporting_Facility": "Facility which reported the Trade",
+                "Trade_Through_Exempt_Indicator": "Indicates if the trade is exempt from Trade Through Rule",
+            }
+            trade_features.update(raw_trade_features)
+
+        if quotes:
+            raw_quote_features = {
+                "Exchange": "Exchange of the Quote",
+                "Symbol": "Symbol of Quote",
+                "Bid_Price": "Best Bid Price on Exchange",
+                "Bid_Size": "Size of the Best Bid on Exchange",
+                "Offer_Price": "Best Offer Price on Exchange",
+                "Offer_Size": "Size of the Best Offer on Exchange",
+                "Quote_Condition": "Condition of the Quote",
+                "Sequence_Number": "Sequence Number of the Quote",
+                "Finra_Bbo_Indicator": "Indicates if the quote is a FINRA BBO",
+                "Source_of_Quote": "Source which generated the Quote",
+                "Best_Bid_Exchange": "Exchange with the National Best Bid",
+                "Best_Bid_Price": "National Best Bid Price",
+                "Best_Bid_Size": "Size of the National Best Bid",
+                "Best_Bid_FINRA_Market_Maker_ID": "FINRA Market Maker ID of the National Best Bid",
+                "Best_Offer_Exchange": "Exchange with the National Best Offer",
+                "Best_Offer_Size": "Size of the National Best Offer",
+                "Best_Offer_FINRA_Market_Maker_ID": "FINRA Market Maker ID of the National Best Offer",
+                "LULD_Indicator": "Indicates if the quote is a LULD quote",
+                "LULD_NBBO_Indicator": "Indicates if the quote is a LULD NBBO",
+                "SIP_Generated_Message_Identifier": "Indentifies associated SIP messages",
+                "FINRA_ADF_MPID_Indicator": "Indicates if the quote is a FINRA ADF quote",
+                "Security_Status_Indicator": "Reports trading suspension status of the security",
+                "Quote_Cancel_Correction": "Indicates if the quote is a correction",
+                "National_BBO_Ind": "Indicates if the quote is a National BBO",
+            }
+            quote_features.update(raw_quote_features)
+
+    if trades:
+        """Generate Trade Features"""
+
+    if quotes:
+        """Generate Quote Features"""
+
+        generated_quote_features = {
+            "Midprice": "Midprice of the Quote",
+            "Midprice_BBO": "Midprice of the National Best Bid and National Best Offer",
+            "Microprice": "Microprice of the Quote",
+            "Microprice_BBO": "Microprice of the National Best Bid and National Best Offer",
+            "Spread": "Difference between the Best Bid and Best Offer",
+            "Spread_BBO": "Difference between the National Best Bid and National Best Offer",
+            "Imbalance": "Ratio between the Best Bid and Best Offer",
+            "Imbalance_BBO": "Ratio between the National Best Bid and National Best Offer",
+        }
+
+        quote_features.update(generated_quote_features)
+
+    if names_only:
+        return list(trade_features.keys()), list(quote_features.keys())
+
+    else:
+        pd.DataFrame.from_dict(trade_features, orient="index", columns=["Description"]), pd.DataFrame.from_dict(
+            quote_features, orient="index", columns=["Description"]
+        )
+
+
+# python scripts/feature_gen/list_features.py
+if __name__ == "__main__":
+    trade_features, quote_features = list_features(raw_data=True, trades=True, quotes=True)
+    print("Trade Features:")
+    print(trade_features)
+
+    print("#############################################")
+
+    print("Quote Features:")
+    print(quote_features)
+
+    print("#############################################")
---
+++ scripts/generate_features.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+from typing import List
+
+import pandas as pd
+from feature_gen.generators import parent_generator
+from feature_gen.list_features import list_features
+
+PATH = "data/"
+
+
+def generate_features(trade_features: List[str] = None, quote_features: List[str] = None, input_file=None):
+
+    # If no features are specified, generate all features (raw and generated)
+    if trade_features is None and quote_features is None:
+        trade_features, quote_features = list_features(names_only=True)
+
+    # get all files in raw_data
+    all_files = os.listdir(f"{PATH}/raw_data")
+
+    assert len(all_files) > 0, "No files in raw_data"
+
+    # case where file is specified
+    if input_file is not None:
+        if input_file not in all_files:
+            raise ValueError(f"{input_file} not in raw_data")
+        else:
+            all_files = [input_file]
+
+    # for each file, generate features
+    for file in all_files:
+        # read in raw data file
+        df = pd.read_csv(f"{PATH}/raw_data/{file}", low_memory=False)
+
+        # generate trade features via parent_generator
+        if trade_features:
+            """Generate Trade Features"""
+
+            features_to_generate = [feature for feature in trade_features if feature not in df.columns]
+
+            df = pd.DataFrame(list(map(lambda x: parent_generator(df, x), features_to_generate))[-1])
+
+            df.to_csv(f"{PATH}/features/{file}_features.csv", index=False)
+
+        # generate quote features via parent_generator
+        if quote_features:
+            """Generate Quote Features"""
+
+            features_to_generate = [feature for feature in quote_features if feature not in df.columns]
+
+            df = pd.DataFrame(list(map(lambda x: parent_generator(df, x), features_to_generate))[-1])
+
+            print(df)
+
+            df.to_csv(f"{PATH}/features/{file}_features.csv", index=False)
+
+
+# python scripts/feature_gen/generate_features.py
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Generate Features")
+    parser.add_argument("--trade_features", nargs="+", help="Features to Generate")
+    parser.add_argument("--quote_features", nargs="+", help="Features to Generate")
+    parser.add_argument("--input_file", default=None, help="Directory of Raw Data")
+
+    args = parser.parse_args()
+
+    # generate all features if no features are specified
+    if args.trade_features is None and args.quote_features is None:
+        generate_features(input_file=args.input_file)
+
+    else:
+        generate_features(args.trade_features, args.quote_features, input_file=args.input_file)
