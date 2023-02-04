0352cf0953c2e2300bdd81a9561b3c66907029d9
Jason Bohne <jbohne822@gmail.com>
2023-02-04 17:36:28 -0500

    improved event reconstruction

--- pipelines/event_reconstruction.py
+++ pipelines/event_reconstruction.py
@@ -1,15 +1,39 @@
-from typing import List
+from typing import Dict, List
+
+import pandas as pd
 
 ### TO-DO: Reconstruct book events before feature generation
 
 
-def reconstruct_book_events(input_files: List[List[str]] = None):
+def reconstruct_book_events(input_files: List[Dict[str]] = None):
     """Reconstruct the book events from the cleaned data"""
 
-    # merge cleaned trades and quotes
+    path_list = []
+    # iterate through each day
+    for day in input_files:
 
-    # sort by participant timestamp
+        print("Reconstructing book events for {}".format(day["trades"]))
 
-    # label mox identifier for each event
+        base_path = day["trades"].split("trades")[0]
 
-    # save to file
+        trades = pd.read_csv(day["trades"], index_col=0)
+        quotes = pd.read_csv(day["quotes"], index_col=0)
+
+        trades["participant_timestamp"] = trades.index
+        quotes["participant_timestamp"] = quotes.index
+
+        all_events = pd.concat([trades, quotes], axis=0)
+
+        # sort by participant timestamp
+        all_events = all_events.sort_values(by=["participant_timestamp"])
+
+        # label mox identifier for each event
+
+        # save to file
+        reconstructed_path = base_path + "reconstructed.csv"
+
+        all_events.to_csv(reconstructed_path)
+
+        path_list.append(reconstructed_path)
+
+    return path_list
