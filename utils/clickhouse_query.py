c3c9140c3c1524b2b368c972ed16e411d16a4552
nbbulldog79 <nbbulldog79@gmail.com>
2023-05-04 17:25:29 -0400

    updates util clickhouse_query

--- utils/clickhouse_query.py
+++ utils/clickhouse_query.py
@@ -2,6 +2,7 @@
 import os
 import clickhouse_connect
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()  # load the contents of the .env file into the environment
 
@@ -15,10 +16,45 @@ db_pass = os.getenv("db_pass")
 # use the credentials to connect to the database
 client = clickhouse_connect.get_client(host=host, port=3306, username=db_user, password=db_pass)
 
-def sql_query(query):
+def get_trades(query):
     """Execute a SQL query and return the results as a list of dictionaries."""
     results = client.command(query)
-    return results
+
+    # Replace 'raw_data' with the variable containing your data as a list
+    raw_data = results
+
+    # Preprocess the raw data
+    rows = []
+    current_row = []
+    for item in raw_data:
+        if '\n' in item:
+            # Split the item at the newline character
+            split_item = item.split('\n')
+
+            # Add the first part of the split item to the current row
+            current_row.append(split_item[0])
+
+            # Add the current row to the list of rows
+            rows.append(current_row)
+
+            # Start a new row with the second part of the split item
+            current_row = [split_item[1]]
+        else:
+            # Add the item to the current row
+            current_row.append(item)
+
+    # Create the DataFrame
+    columns = [
+        'Time', 'Exchange', 'Symbol', 'Sale_Condition', 'Trade_Volume', 'Trade_Price',
+        'Trade_Stop_Stock_Indicator', 'Trade_Correction_Indicator', 'Sequence_Number',
+        'Trade_Id', 'Source_of_Trade', 'Trade_Reporting_Facility', 'Participant_Timestamp',
+        'Trade_Reporting_Facility_TRF_Timestamp', 'Trade_Through_Exempt_Indicator',
+        'Date', 'YearMonth'
+    ]
+
+    trades = pd.DataFrame(rows, columns=columns)
+
+    return trades
 
         
 """Example Queries
@@ -27,4 +63,3 @@ def sql_query(query):
 query = "SELECT * FROM TRADESDB.trades2017view WHERE (Symbol = 'AAPL') AND (Date = '2017-01-05')"
 
 """
-
