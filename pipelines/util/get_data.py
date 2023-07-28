64fcb310e9336397b34252df1ffdcba2c6651519
Michael <nbbulldog79@gmail.com>
2023-07-27 21:59:19 -0400

    cleanup data storage

--- pipelines/util/get_data.py
+++ pipelines/util/get_data.py
@@ -1,13 +1,12 @@
 import paramiko
 from scp import SCPClient
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 def get_trades(symbols, start_date, end_date, row_limit):
-    # load the contents of the .env file into the environment
     load_dotenv()
 
-    # read the credentials from the environment variables
     host = os.getenv("host")
     server_user = os.getenv("server_user")
     server_password = os.getenv("server_password")
@@ -23,9 +22,8 @@ def get_trades(symbols, start_date, end_date, row_limit):
         ssh.connect(host, username=server_user, password=server_password)
 
         for symbol in symbols:
-            # Execute a command to change directory and list files
             command = f'source /home/{server_user}/anaconda3/etc/profile.d/conda.sh && conda activate query_user && cd TAQNYSE-Clickhouse && cd server_helpers && \
-                python3 trade_server_helpers.py "{server_user}" "testpassword321" "{symbol}" "{start_date}" "{end_date}" "{row_limit}"'
+                python3 trade_server_helpers.py "{db_user}" "{db_pass}" "{symbol}" "{start_date}" "{end_date}" "{row_limit}"'
             stdin, stdout, stderr = ssh.exec_command(command)  
 
             print(f"Output for symbol {symbol}:")
@@ -36,27 +34,26 @@ def get_trades(symbols, start_date, end_date, row_limit):
             for line in stderr:
                 print('... ' + line.strip('\n'))
 
-            # SCPCLient takes a paramiko transport as an argument
             scp = SCPClient(ssh.get_transport())
 
-            # fetch the remote file 'trade_results.csv' from the directory 'TAQNYSE-Clickhouse'
-            # and save it to the current directory in your local machine
-            scp.get('TAQNYSE-Clickhouse/trade_results.csv', f'trades_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv')
+            script_path = Path(__file__).resolve().parent
+            project_root = script_path.parent
+            save_path = project_root / "data" / f'trades_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv'
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+
+            scp.get('TAQNYSE-Clickhouse/trade_results.csv', str(save_path))
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
     finally:
         if scp is not None:
-            # close the SCP session
             scp.close()
         ssh.close()
 
 def get_quotes(symbols, start_date, end_date, row_limit):
-    # load the contents of the .env file into the environment
     load_dotenv()
 
-    # read the credentials from the environment variables
     host = os.getenv("host")
     server_user = os.getenv("server_user")
     server_password = os.getenv("server_password")
@@ -72,9 +69,8 @@ def get_quotes(symbols, start_date, end_date, row_limit):
         ssh.connect(host, username=server_user, password=server_password)
 
         for symbol in symbols:
-            # Execute a command to change directory and list files
             command = f'source /home/{server_user}/anaconda3/etc/profile.d/conda.sh && conda activate query_user && cd TAQNYSE-Clickhouse && cd server_helpers && \
-                python3 quote_server_helpers.py "{server_user}" "testpassword321" "{symbol}" "{start_date}" "{end_date}" "{row_limit}"'
+                python3 quote_server_helpers.py "{db_user}" "{db_pass}" "{symbol}" "{start_date}" "{end_date}" "{row_limit}"'
             stdin, stdout, stderr = ssh.exec_command(command)  
 
             print(f"Output for symbol {symbol}:")
@@ -85,18 +81,19 @@ def get_quotes(symbols, start_date, end_date, row_limit):
             for line in stderr:
                 print('... ' + line.strip('\n'))
 
-            # SCPCLient takes a paramiko transport as an argument
             scp = SCPClient(ssh.get_transport())
 
-            # fetch the remote file 'trade_results.csv' from the directory 'TAQNYSE-Clickhouse'
-            # and save it to the current directory in your local machine
-            scp.get('TAQNYSE-Clickhouse/trade_results.csv', f'quotes_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv')
+            script_path = Path(__file__).resolve().parent
+            project_root = script_path.parent
+            save_path = project_root / "data" / f'quotes_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv'
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+
+            scp.get('TAQNYSE-Clickhouse/quote_results.csv', str(save_path))
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
     finally:
         if scp is not None:
-            # close the SCP session
             scp.close()
         ssh.close()
