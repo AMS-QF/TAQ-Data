406b6b0ebd8b764e5c22a8af67e5fe9ec3d612c5
nbbulldog79 <nbbulldog79@gmail.com>
2023-09-06 18:43:24 -0400

    fix quotes get_data

--- utils/get_data.py
+++ utils/get_data.py
@@ -75,7 +75,7 @@ def get_quotes(symbols, start_date, end_date, row_limit):
 
         for symbol in symbols:
             # Execute a command to change directory and list files
-            command = f'source /home/{server_user}/anaconda3/etc/profile.d/conda.sh && conda activate query_user && cd TAQNYSE-Clickhouse && cd server_helpers && \
+            command = f'source /opt/anaconda3/etc/profile.d/conda.sh && conda activate query_user && cd TAQNYSE-Clickhouse && cd server_helpers && \
                 python3 quote_server_helpers.py "{server_user}" "testpassword321" "{symbol}" "{start_date}" "{end_date}" "{row_limit}"'
             stdin, stdout, stderr = ssh.exec_command(command)
 
