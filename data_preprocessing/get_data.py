c787ec5497d900a7088492858360dfd48ac7b5aa
nbbulldog79 <nbbulldog79@gmail.com>
2023-09-06 21:07:15 -0400

    fix path

--- data_preprocessing/get_data.py
+++ data_preprocessing/get_data.py
@@ -43,7 +43,9 @@ def get_trades(symbols, start_date, end_date, row_limit):
 
             # fetch the remote file 'trade_results.csv' from the directory 'TAQNYSE-Clickhouse'
             # and save it to the data directory in the pipelines folder
-            scp.get('TAQNYSE-Clickhouse/trade_results.csv', f'../pipelines/data/trades_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv')
+            local_file_path = f'../pipelines/data/trades_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv'
+        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+        scp.get('TAQNYSE-Clickhouse/trade_results.csv', local_file_path)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -92,7 +94,9 @@ def get_quotes(symbols, start_date, end_date, row_limit):
 
             # fetch the remote file 'trade_results.csv' from the directory 'TAQNYSE-Clickhouse'
             # and save it to the data directory in the pipelines folder
-            scp.get('TAQNYSE-Clickhouse/quote_results.csv', f'../pipelines/data/quotes_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv')
+            local_file_path = f'../pipelines/data/quotes_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv'
+        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+        scp.get('TAQNYSE-Clickhouse/quote_results.csv', local_file_path)
 
 
     except Exception as e:
@@ -142,7 +146,9 @@ def get_ref(symbols, start_date, end_date, row_limit):
 
             # fetch the remote file 'trade_results.csv' from the directory 'TAQNYSE-Clickhouse'
             # and save it to the data directory in the pipelines folder
-            scp.get('TAQNYSE-Clickhouse/refdata_results.csv', f'../pipelines/data/ref_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv')
+            local_file_path = f'../pipelines/data/ref_{symbol}_{start_date.replace("-", "")}-{end_date.replace("-", "")}.csv'
+        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+        scp.get('TAQNYSE-Clickhouse/refdata_results.csv', local_file_path)
 
     except Exception as e:
         print(f"An error occurred: {e}")
